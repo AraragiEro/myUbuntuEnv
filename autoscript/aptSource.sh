e55704239990d356af552f12e43241987172edf3
AraragiEro <zhucct@126.com>
2021-08-21 19:04:32 +0800

    2021.8.21 	修改：     autoscript/aptSource.sh 	修改：     init.sh

--- autoscript/aptSource.sh
+++ autoscript/aptSource.sh
@@ -2,7 +2,9 @@
 
 source $g_envPakDir/autoscript/log.sh
 
-function detecSys(){
+Ver="error"
+
+function detecSysType(){
 	sysType=$(cat /etc/issue | tr 'A-Z' 'a-z' | grep 'ubuntu')
 	if [ -z "$sysType" ]
 	then
@@ -10,6 +12,83 @@ function detecSys(){
 	fi
 }
 
-function changeSource(){
-	detecSys
+function detecSysVer(){
+	if [ ! -z "$(cat /etc/issue | grep "20.04")" ]; then
+		Ver="20.04"
+	elif [ ! -z $(cat /etc/issue | grep "18.04") ]; then
+		Ver="18.04"
+	else
+		exit 1
+	fi
 }
+
+function source18(){
+	LOG info "system is Ubuntu 18.04, change source to aliMirrors!"
+	cat > $g_envPakDir/sourceTmp.tmp << sour
+deb http://mirrors.aliyun.com/ubuntu/ bionic main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic main restricted universe multiverse
+
+deb http://mirrors.aliyun.com/ubuntu/ bionic-security main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic-security main restricted universe multiverse
+
+deb http://mirrors.aliyun.com/ubuntu/ bionic-updates main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic-updates main restricted universe multiverse
+
+deb http://mirrors.aliyun.com/ubuntu/ bionic-proposed main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic-proposed main restricted universe multiverse
+
+deb http://mirrors.aliyun.com/ubuntu/ bionic-backports main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic-backports main restricted universe multiverse
+
+sour
+	sudo rm -rf /etc/apt/sources.list
+	sudo cp $g_envPakDir/sourceTmp.tmp /etc/apt/sources.list
+}
+
+function source20(){
+	LOG info "system is Ubuntu 20.04,change source to aliMirrors!"
+	cat > $g_envPakDir/sourceTmp.tmp << sour 
+deb http://mirrors.aliyun.com/ubuntu/ focal main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ focal main restricted universe multiverse
+
+deb http://mirrors.aliyun.com/ubuntu/ focal-security main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ focal-security main restricted universe multiverse
+
+deb http://mirrors.aliyun.com/ubuntu/ focal-updates main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ focal-updates main restricted universe multiverse
+
+deb http://mirrors.aliyun.com/ubuntu/ focal-proposed main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ focal-proposed main restricted universe multiverse
+
+deb http://mirrors.aliyun.com/ubuntu/ focal-backports main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ focal-backports main restricted universe multiverse
+sour
+	sudo rm -rf /etc/apt/sources.list
+	sudo cp $g_envPakDir/sourceTmp.tmp /etc/apt/sources.list
+
+}
+
+function changeSource(){
+	detecSysType
+	detecSysVer
+	if [ $? -ne 0 ];then
+		LOG err "Error Ubuntu Version!"
+		exit 1
+	else
+		sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak
+	fi
+
+	if [ $Ver == "20.04" ];
+	then
+		source20	
+	elif [ $Ver == "18.04" ]; then
+		source18
+	fi
+	if [ $? -ne 0 ]; then
+		rm -rf $g_envPakDir/*.tmp > /dev/null
+		exit 1
+	fi
+	rm -rf $g_envPakDir/*.tmp > /dev/null
+}
+
+
--- init.sh
+++ init.sh
@@ -4,13 +4,32 @@ export g_homeDir="~/"
 export g_envPakDir=$(cd $(dirname $0);pwd)
 
 source ./autoscript/aptSource.sh
+source ./autoscript/log.sh
+
+function checkEnv(){
+	#检查有哪些软件没装
+	# vim zsh tmux
+	return 0
+}
 
 function initEnv(){
-	changeSource
+	# 更换软件源
+	# func in ./autoscript/aptSource.sh
+	changeSource 
+	if [ $? -ne 0 ];then
+		LOG err "fail to change apt source!"
+		exit 1
+	fi
+
+
 }
 
 function main(){
+	#1.环境检查：
+	checkEnv
+	# 2.环境准备：源替换，软件包更新
 	initEnv
+	
 }
 
 main
